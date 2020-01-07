8224554b8fdd0cd21cf730c0d574b87dbe1abcdc
Philippe <philippe@henaff.me>
2020-01-07 07:14:09 +0100

    Add some tools

---
+++ tools/crypt.php
@@ -0,0 +1,46 @@
+<?php
+
+if (isset($_POST['login']) AND isset($_POST['pass']))
+
+{
+
+    $login = $_POST['login'];
+
+    $pass_crypte = password_hash($_POST['pass'], PASSWORD_DEFAULT); // On crypte le mot de passe
+
+
+    echo '<p>Ligne à copier dans le .htpasswd :<br />' . $login . ':' . $pass_crypte . '</p>';
+
+}
+
+
+else // On n'a pas encore rempli le formulaire
+
+{
+
+?>
+
+
+<p>Entrez votre login et votre mot de passe pour le crypter.</p>
+
+
+<form method="post">
+
+    <p>
+
+        Login : <input type="text" name="login"><br />
+
+        Mot de passe : <input type="text" name="pass"><br /><br />
+
+    
+
+        <input type="submit" value="Crypter !">
+
+    </p>
+
+</form>
+
+
+<?php
+
+}
